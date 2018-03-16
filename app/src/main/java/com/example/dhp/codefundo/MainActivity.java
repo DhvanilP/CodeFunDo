639bf92dc959e06b7fdca8257ff3816ead550424
DhvanilP <dhvanilhparikh@gmail.com>
2018-03-16 17:04:10 +0530

    createGroup functioning

--- app/src/main/java/com/example/dhp/codefundo/MainActivity.java
+++ app/src/main/java/com/example/dhp/codefundo/MainActivity.java
@@ -1,20 +1,41 @@
 package com.example.dhp.codefundo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import java.io.*;
-import android.app.*;
-import android.content.*;
-import android.net.*;
-import android.os.*;
-import android.view.*;
-import android.graphics.*;
-import android.widget.*;
-import android.provider.*;
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.microsoft.projectoxford.face.contract.Face;
+import com.microsoft.projectoxford.face.contract.FaceRectangle;
 
-import com.microsoft.projectoxford.face.*;
-import com.microsoft.projectoxford.face.contract.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +49,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button)findViewById(R.id.button1);
+
+//        createGroup("students");
+
+
+        Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,10 +85,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Detect faces by uploading face images
-// Frame faces after detection
 
-    private void detectAndFrame(final Bitmap imageBitmap)
-    {
+    private void detectAndFrame(final Bitmap imageBitmap) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
         ByteArrayInputStream inputStream =
@@ -80,8 +103,7 @@ public class MainActivity extends AppCompatActivity {
                                     false,        // returnFaceLandmarks
                                     null           // returnFaceAttributes: a string like "age, gender"
                             );
-                            if (result == null)
-                            {
+                            if (result == null) {
                                 publishProgress("Detection Finished. Nothing detected");
                                 return null;
                             }
@@ -94,22 +116,25 @@ public class MainActivity extends AppCompatActivity {
                             return null;
                         }
                     }
+
                     @Override
                     protected void onPreExecute() {
                         //TODO: show progress dialog
                         detectionProgressDialog.show();
                     }
+
                     @Override
                     protected void onProgressUpdate(String... progress) {
                         //TODO: update progress
                         detectionProgressDialog.setMessage(progress[0]);
                     }
+
                     @Override
                     protected void onPostExecute(Face[] result) {
                         //TODO: update face frames
                         detectionProgressDialog.dismiss();
                         if (result == null) return;
-                        ImageView imageView = (ImageView)findViewById(R.id.imageView1);
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView1);
                         imageView.setImageBitmap(drawFaceRectanglesOnBitmap(imageBitmap, result));
                         imageBitmap.recycle();
                     }
@@ -139,4 +164,50 @@ public class MainActivity extends AppCompatActivity {
         }
         return bitmap;
     }
+
+    // Frame faces after detection
+    private void createGroup(String students) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        HttpClient httpClient = new DefaultHttpClient();
+        try {
+
+            // The valid characters for the ID below include numbers, English letters in lower case, '-', and '_'.
+            // The maximum length of the personGroupId is 64.
+
+            // NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+            //   For example, if you obtained your subscription keys from westus, replace "westcentralus" in the
+            //   URL below with "westus".
+            URIBuilder builder = new URIBuilder("https://southeastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/" +
+                    students);
+
+            URI uri = builder.build();
+            HttpPut request = new HttpPut(uri);
+
+            // Request headers. Replace the example key with your valid subscription key.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", "eb5c5e259ead4741b0e2792b17fbc98c");
+            // Request body. The name field is the display name you want for the group (must be under 128 characters).
+            // The size limit for what you want to include in the userData field is 16KB.
+            String body = "{ \"name\":\"My Group to try\",\"userData\":\"User-provided data attached to the PersonGroup.\" }";
+            StringEntity reqEntity = new StringEntity(body);
+
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpClient.execute(request);
+            Log.d("custom", response.toString());
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                System.out.println(EntityUtils.toString(entity));
+                System.out.println("try success");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("try failed");
+
+        }
+    }
+
+
 }
