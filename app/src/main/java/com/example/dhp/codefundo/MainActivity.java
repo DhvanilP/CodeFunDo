1d9ad2d4d573f07f5a3c6cf79b0208aeaaf8a8d4
suyash0103 <suyashghuge@gmail.com>
2018-03-16 14:41:21 +0530

    initial commit

--- app/src/main/java/com/example/dhp/codefundo/MainActivity.java
+++ app/src/main/java/com/example/dhp/codefundo/MainActivity.java
@@ -14,9 +14,39 @@ import android.provider.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int PICK_IMAGE = 1;
+    private ProgressDialog detectionProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                gallIntent.setType("image/*");
+                startActivityForResult(Intent.createChooser(gallIntent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+        detectionProgressDialog = new ProgressDialog(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
