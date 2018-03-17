6339e6b82838aae7cd273e27c322c68094bdab11
DhvanilP <dhvanilhparikh@gmail.com>
2018-03-17 14:39:23 +0530

    check for existing user

--- app/src/main/java/com/example/dhp/codefundo/CreatePerson.java
+++ app/src/main/java/com/example/dhp/codefundo/CreatePerson.java
@@ -3,15 +3,20 @@ package com.example.dhp.codefundo;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.microsoft.projectoxford.face.FaceServiceClient;
 import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.microsoft.projectoxford.face.contract.Person;
 import com.microsoft.projectoxford.face.rest.ClientException;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class CreatePerson extends AppCompatActivity {
     EditText personName;
@@ -20,6 +25,8 @@ public class CreatePerson extends AppCompatActivity {
     Button submitPerson;
     ProgressDialog detectionProgressDialog;
     private FaceServiceClient faceServiceClient;
+    HashMap<String, Person> personsData;
+    Person[] persons;
 
 
     @Override
@@ -29,7 +36,33 @@ public class CreatePerson extends AppCompatActivity {
         personName = findViewById(R.id.personName);
         personGroup = findViewById(R.id.personGroup);
         personroll = findViewById(R.id.personRoll);
+        personsData = new HashMap<>();
         faceServiceClient = new FaceServiceRestClient(MainActivity.SERVER_HOST, MainActivity.SUBSCRIPTION_KEY);
+
+//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//        StrictMode.setThreadPolicy(policy);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    persons = faceServiceClient.getPersons("testing");
+                    for (Person person : persons) {
+                        personsData.put(person.userData, person);
+                    }
+                    Iterator it = personsData.entrySet().iterator();
+                    while (it.hasNext()) {
+                        HashMap.Entry pair = (HashMap.Entry) it.next();
+                        Log.v("addddddddd", pair.getKey().toString());
+//                Log.v("addddddddd", pair.getValue().toString());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+
 
         submitPerson = findViewById(R.id.submitPerson);
         submitPerson.setOnClickListener(new View.OnClickListener() {
@@ -46,29 +79,36 @@ public class CreatePerson extends AppCompatActivity {
 
     }
 
+
     private void createPerson() throws IOException, ClientException {
         detectionProgressDialog = new ProgressDialog(this);
         detectionProgressDialog.setMessage("Creating person");
         detectionProgressDialog.show();
         detectionProgressDialog.setCanceledOnTouchOutside(false);
 
-        Thread background = new Thread() {
-            @Override
-            public void run() {
-                String name = personName.getText().toString();
-                String group = personGroup.getText().toString();
-                String rollNumber = personroll.getText().toString();
-                try {
-                    faceServiceClient.createPerson(group, name, rollNumber);
-                    detectionProgressDialog.dismiss();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    detectionProgressDialog.dismiss();
-                }
 
-            }
-        };
-        background.start();
+        String rollNumber = personroll.getText().toString();
+        if (personsData.get(rollNumber) == null) {
+            Thread background = new Thread() {
+                @Override
+                public void run() {
+                    String name = personName.getText().toString();
+                    String group = personGroup.getText().toString();
+                    String rollNumber = personroll.getText().toString();
+                    try {
+                        faceServiceClient.createPerson(group, name, rollNumber);
+                        detectionProgressDialog.dismiss();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        detectionProgressDialog.dismiss();
+                    }
+                }
+            };
+            background.start();
+        } else {
+            Toast.makeText(getApplicationContext(), "Person exists", Toast.LENGTH_SHORT).show();
+            detectionProgressDialog.dismiss();
+        }
     }
 
 }
