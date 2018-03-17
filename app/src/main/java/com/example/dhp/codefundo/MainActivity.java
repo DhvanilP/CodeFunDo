b75b9fea7621a15d90977601c50be920688afdea
bordia98 <bhavyabordia@gmail.com>
2018-03-17 23:31:25 +0530

    First Page Added

--- app/src/main/java/com/example/dhp/codefundo/MainActivity.java
+++ app/src/main/java/com/example/dhp/codefundo/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.dhp.codefundo;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -40,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private static FaceServiceClient faceServiceClient;
     private final int PICK_IMAGE = 1;
     ProgressDialog  detectionProgressDialog;
+    String groupid;
 
     private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
         Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
@@ -100,6 +99,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(getIntent().hasExtra("groupId")) {
+            groupid = getIntent().getStringExtra("groupId");
+        }
         detectionProgressDialog = new ProgressDialog(this);
         detectionProgressDialog.setMessage("Creating person");
         detectionProgressDialog.setCanceledOnTouchOutside(false);
@@ -126,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CreatePerson.class);
+                intent.putExtra("groupId",groupid);
                 startActivity(intent);
             }
         });
@@ -236,21 +240,6 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         background.start();
-    }
-
-    public String getRealPathFromURI(Context context, Uri contentUri) {
-        Cursor cursor = null;
-        try {
-            String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
     }
 
 }
