183d01bf0f9a9f525e44239bd548e3cae13f8c47
suyash0103 <suyashghuge@gmail.com>
2018-03-20 17:21:07 +0530

    framework for sending email-2

--- app/src/main/java/com/example/dhp/codefundo/EmailActivity.java
+++ app/src/main/java/com/example/dhp/codefundo/EmailActivity.java
@@ -1,13 +1,43 @@
 package com.example.dhp.codefundo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class EmailActivity extends AppCompatActivity {
+public class EmailActivity extends Activity {
+    Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
+
+        send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new Thread(new Runnable() {
+                    public void run() {
+                        try {
+                            GMailSender sender = new GMailSender(
+                                    "suyashghuge@gmail.com",
+                                    "roboticmind");
+
+                            sender.addAttachment(Environment.getExternalStorageDirectory().getPath() + "/image.jpg");
+                            sender.sendMail("Test mail", "This mail has been sent from android app along with attachment",
+                                    "suyashghuge@gmail.com",
+                                    "cfdwhitewalkers@gmail.com");
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }).start();
+            }
+        });
     }
 }
